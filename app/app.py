9f8d4a1d26ba72393a12f21020362070ecd18207
LOCOCbKA <maryankov00@gmail.com>
2026-06-16 16:29:51 +0300

    Add app and Dockerfile

---
+++ app/app.py
@@ -0,0 +1,19 @@
+import psycopg2
+import os
+
+def main():
+    conn = psycopg2.connect(
+        host=os.getenv('DB_HOST', 'db'),
+        database=os.getenv('DB_NAME', 'testdb'),
+        user=os.getenv('DB_USER', 'testuser'),
+        password=os.getenv('DB_PASSWORD', 'testpass')
+    )
+    cur = conn.cursor()
+    cur.execute('SELECT version();')
+    version = cur.fetchone()[0]
+    print(f'Connected to PostgreSQL: {version}')
+    cur.close()
+    conn.close()
+
+if __name__ == '__main__':
+    main()
